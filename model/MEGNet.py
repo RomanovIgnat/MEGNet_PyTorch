81ee69a09a572a7cf9ce7e041c910a2d9016c16f
RomanovIgnat <romanov.ignat.p@gmail.com>
2022-03-03 03:32:58 +0300

    third

--- model/MEGNet.py
+++ model/MEGNet.py
@@ -9,6 +9,7 @@ class MEGNet(nn.Module):
         super().__init__()
         self.m1 = MegnetModule(100, 1, 2)
         self.m2 = MegnetModule(32, 32, 32)
+        self.m3 = MegnetModule(32, 32, 32)
         self.se = Set2Set(32, 1)
         self.sv = Set2Set(32, 1)
         self.hiddens = nn.Sequential(
@@ -22,6 +23,7 @@ class MEGNet(nn.Module):
     def forward(self, x, edge_index, edge_attr, state, batch, bond_batch):
         x, edge_attr, state = self.m1(x, edge_index, edge_attr, state, batch, bond_batch)
         x, edge_attr, state = self.m2(x, edge_index, edge_attr, state, batch, bond_batch)
+        x, edge_attr, state = self.m3(x, edge_index, edge_attr, state, batch, bond_batch)
         x = self.sv(x, batch)
         edge_attr = self.se(edge_attr, bond_batch)
         tmp = torch.cat((x, edge_attr, state), 1)
